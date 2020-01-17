44da95a7e5d0e455582b290921b3d0a5ac2348d3
Ewerton Luna <ewerton.al@gmail.com>
2020-01-17 16:18:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const data = require('./data.json');
+const { projects } = require('./data.json');
 
 const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.locals.projects = projects;
+  res.render('index');
+});
