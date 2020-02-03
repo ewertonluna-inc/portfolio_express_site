3252305dea53ab76f499f14aba7b813650936750
Ewerton Luna <ewerton.al@gmail.com>
2020-02-03 10:33:36 -0300

    Add error handlers

--- routes/projects.js
+++ routes/projects.js
@@ -7,6 +7,15 @@ const { projects } = require('../data.json');
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
+  if (isNaN(id)) {
+    const error = new Error('The route requires a numbered id');
+    error.status = 404;
+    return next(error);
+  } else if (id > projects.length - 1) {
+    const error = new Error('Project doesn\'t exist');
+    error.status = 404;
+    return next(error);
+  }
   const project = projects[id];
   res.render('project', {project});
 });
